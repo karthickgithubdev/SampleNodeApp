88e2f9fca9100063777e45211514f37dd0548b37
Thiyagarajan <karthikaikumaran.thiyagarajan@capgemini.com>
2019-03-17 15:25:48 +0530

    New nodejs app

---
+++ SampleApp/app.js
@@ -0,0 +1,11 @@
+const http = require("http");
+
+http.createServer(function (req, res) {
+	
+	res.writeHead(200, {'Content-Type':'text/plain'});
+	
+	res.end('Hello World !');
+	
+}).listen(8081);
+
+console.log("Server running in http://127.0.0.1:8081/");
